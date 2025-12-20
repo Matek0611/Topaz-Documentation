5260fff5f6e3e0e346c149246ab0cbc26dcc6398
Marcin <matiowo@wp.pl>
2025-12-20 01:53:26 +0100

    fix: compat table items

--- docs/index.md
+++ docs/index.md
@@ -8,8 +8,12 @@
         {"feature": "test", "support": "full", "version": "10", "timeline": [] }
     ],
     "macOS": [
+        {"feature": "test", "support": "partial", "version": "10", "timeline": [] },
         {"feature": "test2", "support": "full", "version": "26", "timeline": ["xx"] }
     ],
-    "Linux": []
+    "Linux": [
+        {"feature": "test", "support": "experimental", "version": "1", "timeline": [] },
+        {"feature": "test2", "support": "deprecated", "version": "1", "timeline": [] }
+    ]
 }
 }}} 
--- topaz_extensions/topaz_extensions/compatibility_table.py
+++ topaz_extensions/topaz_extensions/compatibility_table.py
@@ -22,25 +22,7 @@ class CompatibilityBlockProcessor(BlockProcessor):
     def test(self, parent, block):
         return block.startswith('{{{compatibility:')
     
-    def generate_legend(self, element: etree.Element):
-        container = etree.SubElement(element, 'div', {'class': 'compat-table-legend'})
-        items = [
-            (icons.ICON_FULL_COMPATIBILITY, 'Full support.'),
-            (icons.ICON_PARTIAL_COMPATIBILITY, 'Partial support.'),
-            (icons.ICON_NO_COMPATIBILITY, 'No support.'),
-            (icons.ICON_DEPRECATED, 'Deprecated. Do not use in new scripts.'),
-            (icons.ICON_EXPERIMENTAL, 'Experimental. Expect behavior to change in the future.'),
-            (icons.ICON_DOTS, 'See implementation notes.')
-        ]
-
-        for item in items:
-            citem = etree.SubElement(container, 'div', {'class': 'compat-table-legend-item'})
-            etree.SubElement(citem, 'div', {'class': 'compat-table-icon'}).text = item[0]
-            etree.SubElement(citem, 'span').text = item[1]
-
-    def run(self, parent, blocks):
-        etree.SubElement(parent, 'h2').text = 'Operating system compatibility'
-
+    def get_raw_json(self, blocks):
         block = blocks.pop(0)
 
         lines = block.splitlines()
@@ -51,15 +33,20 @@ class CompatibilityBlockProcessor(BlockProcessor):
                 break
             json_lines.append(line)
 
-        raw_json = '\n'.join(json_lines)
+        return '\n'.join(json_lines)
+    
+    def generate_compat_item(self, parent, icon, title, add_more, color_class):
+        parent.set('class', color_class)
+        compat_item = etree.SubElement(parent, 'div', {'class': 'compat-table-item'})
+        
+        etree.SubElement(compat_item, 'span').text = icon
 
-        try:
-            data = json.loads(raw_json)
-        except json.JSONDecodeError as e:
-            pre = etree.SubElement(parent, 'em')
-            pre.text = f'Invalid compatibility JSON: {e}'
-            return
-
+        if title:
+            etree.SubElement(compat_item, 'span').text = title
+        if add_more:
+            etree.SubElement(compat_item, 'span', {'style': 'color: #555; cursor: pointer;'}).text = icons.ICON_DOTS
+    
+    def generate_table(self, parent, data):
         table = etree.SubElement(etree.SubElement(parent, 'div', {'class': 'compat-block'}), 'table', {'class': 'compat-table'})
         thead = etree.SubElement(table, 'thead')
         tbody = etree.SubElement(table, 'tbody')
@@ -89,11 +76,50 @@ class CompatibilityBlockProcessor(BlockProcessor):
                 tdinfo = etree.SubElement(trbody, 'td')
 
                 if system not in fvalue.keys():
-                    tdinfo.text = 'none'
+                    self.generate_compat_item(tdinfo, icons.ICON_NO_COMPATIBILITY, 'No', False, 'compat-no-support')
                     continue
 
-                tdinfo.text = 'x'
+                fivalue = fvalue[system]
 
+                match fivalue.get('support', ''):
+                    case 'full': 
+                        self.generate_compat_item(tdinfo, icons.ICON_FULL_COMPATIBILITY, fivalue.get('version', 'Yes'), fivalue.get('timeline', ''), 'compat-supported')
+                    case 'partial':
+                        self.generate_compat_item(tdinfo, icons.ICON_PARTIAL_COMPATIBILITY, fivalue.get('version', 'Partial'), fivalue.get('timeline', ''), 'compat-partial-support')
+                    case 'deprecated':
+                        self.generate_compat_item(tdinfo, icons.ICON_DEPRECATED, fivalue.get('version', 'Obsolete'), fivalue.get('timeline', ''), 'compat-deprecated')
+                    case 'experimental':
+                        self.generate_compat_item(tdinfo, icons.ICON_EXPERIMENTAL, fivalue.get('version', 'New'), fivalue.get('timeline', ''), 'compat-experimental')
+                    case _:
+                        self.generate_compat_item(tdinfo, icons.ICON_NO_COMPATIBILITY, fivalue.get('version', 'No'), fivalue.get('timeline', ''), 'compat-no-support')
+    
+    def generate_legend(self, element: etree.Element):
+        container = etree.SubElement(element, 'div', {'class': 'compat-table-legend'})
+        items = [
+            (icons.ICON_FULL_COMPATIBILITY, 'Full support.'),
+            (icons.ICON_PARTIAL_COMPATIBILITY, 'Partial support.'),
+            (icons.ICON_NO_COMPATIBILITY, 'No support.'),
+            (icons.ICON_DEPRECATED, 'Deprecated. Do not use in new scripts.'),
+            (icons.ICON_EXPERIMENTAL, 'Experimental. Expect behavior to change in the future.'),
+            (icons.ICON_DOTS, 'See implementation notes.')
+        ]
+
+        for item in items:
+            citem = etree.SubElement(container, 'div', {'class': 'compat-table-legend-item'})
+            etree.SubElement(citem, 'div', {'class': 'compat-table-icon'}).text = item[0]
+            etree.SubElement(citem, 'span').text = item[1]
+
+    def run(self, parent, blocks):
+        etree.SubElement(parent, 'h2').text = 'Operating system compatibility'
+
+        try:
+            data = json.loads(self.get_raw_json(blocks))
+        except json.JSONDecodeError as e:
+            info = etree.SubElement(parent, 'em')
+            info.text = f'Invalid compatibility JSON data ({e}).'
+            return
+
+        self.generate_table(parent, data)
         self.generate_legend(parent)
 
 
